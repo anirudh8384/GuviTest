158f04dd461eae98a06f7a11aeb14716abd2c103
Anirudh Singh <anirudh.dfx@gmail.com>
2017-02-14 18:06:31 +0530

    Question2
    Solution to even or odd

---
+++ question2.java
@@ -0,0 +1,28 @@
+
+		import java.util.*;
+		import java.lang.*;
+		import java.io.*;
+
+
+public class question{
+	
+			public static void main (String[] args) throws java.lang.Exception
+			{
+				Scanner sc = new Scanner(System.in);
+				int no = sc.nextInt();
+				if(no %2 == 0){
+					System.out.println(no);
+					System.out.println("even");
+					}
+					
+					else{
+					System.out.println(no);
+					System.out.println("odd");
+					}
+				
+		
+		}
+
+	
+
+}
