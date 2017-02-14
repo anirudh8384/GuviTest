5709f72e71c189940e45c05f09fc877255225383
Anirudh Singh <anirudh.dfx@gmail.com>
2017-02-14 17:45:08 +0530

    Question1
    Check whether a number is positive or negative or zero

---
+++ question.java
@@ -0,0 +1,31 @@
+
+		import java.util.*;
+		import java.lang.*;
+		import java.io.*;
+
+
+public class question{
+	
+			public static void main (String[] args) throws java.lang.Exception
+			{
+				Scanner sc = new Scanner(System.in);
+				int no = sc.nextInt();
+				if(no > 0){
+					System.out.println(no);
+					System.out.println("Positive");
+					}
+					else if(no < 0){
+					System.out.println(no);
+					System.out.println("output Negative");	
+					}
+					else{
+					System.out.println(no);
+					System.out.println("output zero");
+					}
+				
+		
+		}
+
+	
+
+}
